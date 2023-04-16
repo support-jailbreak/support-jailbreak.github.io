8d5e359cc77ac1037d533bbb7b50de438ab0b51f
support-jailbreak <jailbreaks415@gmail.com>
2023-04-16 23:35:43 +0700

    -

--- update.sh
+++ update.sh
@@ -50,12 +50,12 @@ echo "{}]" >> all.pkgs
 }
 echo "------------------"
 echo "Building Packages...."
-apt-ftparchive packages ./debfiles > ./Packages;
+apt-ftparchive packages ./debs > ./Packages;
 #sed -i -e '/^SHA/d' ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
 echo "------------------"
 echo "Building Release...."
-printf "Origin: BVN MOBILE SERVICES\nLabel: Apple-Support\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: BVN MOBILE SERVICES\nLabel: Apple-Support\nSuite: stable\nVersion: 1.1\nCodename: ios\nArchitecture: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 
 echo "------------------"
 echo "Done!"
